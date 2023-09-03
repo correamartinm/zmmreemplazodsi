2f92df030d67fade54454ceb46cafc58af24f359
correamartinm <correamartinm@gmail.com>
2023-09-02 21:55:48 -0300

    ok

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,51 +1,47 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
 
-        return Controller.extend("morixe.zmmreemplazodsi.controller.MainView", {
-            onInit: function () {
+    return Controller.extend("morixe.zmmreemplazodsi.controller.MainView", {
+      onInit: function () {},
 
-            },
+      onReiniciar: function () {
+        let oMockModel = this.getOwnerComponent().getModel("mockdata");
 
-            onReiniciar: function () {
-                let oMockModel = this.getOwnerComponent().getModel("mockdata");
+        oMockModel.setProperty("/MainButtons", false);
+      },
 
-                oMockModel.setProperty("/MainButtons", false);
-            },
+      onGoOtEntranteOLV: function () {
+        let oMockModel = this.getOwnerComponent().getModel("mockdata"),
+          oCenter = oMockModel.setProperty("/ActiveCenter", "OLV");
+        oMockModel.setProperty("/MainButtons", true);
+      },
+      onGoOtEntranteCBA: function () {
+        let oMockModel = this.getOwnerComponent().getModel("mockdata"),
+          oCenter = oMockModel.setProperty("/ActiveCenter", "CBA");
+        oMockModel.setProperty("/MainButtons", true);
+      },
 
-            onGoOtEntranteOLV: function () {
+      onGoOtEntrante: function () {
+        this.getOwnerComponent().getTargets().display("TargetOtEntrante");
+      },
+      onGoOtSaliente: function () {
+        this.getOwnerComponent().getTargets().display("TargetOtSaliente");
+      },
+      onGoMovimiento: function () {
+        this.getOwnerComponent().getTargets().display("TargetMovimiento");
+      },
+      onGoRemanejo: function () {
+        this.getOwnerComponent().getTargets().display("TargetRemanejo");
+      },
 
-                let oMockModel = this.getOwnerComponent().getModel("mockdata"),
-                oCenter = oMockModel.setProperty("/ActiveCenter", "OLV");
-                oMockModel.setProperty("/MainButtons", true);
-                
-            },
-            onGoOtEntranteCBA: function () {
-                let oMockModel = this.getOwnerComponent().getModel("mockdata"),
-                oCenter = oMockModel.setProperty("/ActiveCenter", "CBA");
-                oMockModel.setProperty("/MainButtons", true);
-                
-            },
-
-
-            onGoOtEntrante: function () {
-                this.getOwnerComponent().getTargets().display("TargetOtEntrante");
-            },
-            onGoOtSaliente: function () {
-                this.getOwnerComponent().getTargets().display("TargetOtSaliente");
-            },
-            onGoMovimiento: function () {
-                this.getOwnerComponent().getTargets().display("TargetMovimiento");
-            },
-
-            onGoMain: function () {
-                this.getOwnerComponent().getTargets().display("TargetMainView");
-            },
-
-        });
+      onGoMain: function () {
+        this.getOwnerComponent().getTargets().display("TargetMainView");
+      },
     });
+  }
+);
---
+++ webapp/controller/Remanejo.controller.js
@@ -0,0 +1,49 @@
+sap.ui.define(
+  ["./BaseController"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("morixe.zmmreemplazodsi.controller.Remanejo", {
+      onInit: function () {
+        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+        let oTarget = oRouter.getTarget("TargetMovimiento");
+        oTarget.attachDisplay(this._onObjectMatched, this);
+      },
+
+
+      _onObjectMatched: function (evt) {
+        this._onFocusControl(this.byId("idMovPalletInput"));        
+      },
+
+      _onResetData: function () {
+        let oMockModel = this.getOwnerComponent().getModel("mockdata");
+        this._onUpdateJsonModel(oMockModel, "/movOT", "");
+        this._onUpdateJsonModel(oMockModel, "/movOTPosicion", "");
+        this._onUpdateJsonModel(oMockModel, "/movOTMaterial", "");
+        this._onUpdateJsonModel(oMockModel, "/movOTMaterialDesc", "");
+        this._onUpdateJsonModel(oMockModel, "/movOTCantidad", "");
+        this._onUpdateJsonModel(oMockModel, "/movOTUnidad", "");
+        this._onUpdateJsonModel(oMockModel, "/movOTPallet", "");
+        this._onUpdateJsonModel(oMockModel, "/movOTValidate", false);
+      },
+
+      _onGotoMainMenu: function () {
+        let sMessage = this._i18n().getText("msgvolver"),
+          sMessageTitle = this._i18n().getText("msgconsulta");
+
+        this._onShowMsgBoxConfirm(sMessage, sMessageTitle).then((rta) => {
+          if (rta === "OK") {
+            this._onResetData();
+            this.onGoMain();
+          }
+        });
+      },
+
+
+    });
+  }
+);
